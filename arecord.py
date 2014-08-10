5d2c4b1cd06a683aa078b128f021a6160485d9b7
Carl Winkler <carl@carlossus.com>
2014-08-10 11:48:14 +0000

    introduced variable duration

--- arecord.py
+++ arecord.py
@@ -5,21 +5,26 @@ if len(sys.argv) == 1:
 	exit()
 
 Filename = sys.argv[1]
+Seconds = 0
 Count = 0
 
+if sys.argv[2] is not None:
+	Seconds = sys.argv[2]
+
 def main():
-	global Filename
-	global Count
+	global Filename, Count, Seconds
 
 	validate_filename()
 	print("Recording to file: " + Filename + "_x.wav")
+	if Seconds > 0:
+		print("Recording for " + str(Seconds) + " seconds.")
 	record()
 	exit()
 
 def record():
 	global Filename
 	global Count
-	returnCode = subprocess.call("arecord -D plughw:1  --rate 140000 " + get_filename(), shell=True)
+	returnCode = subprocess.call(get_record_string(), shell=True)
 	if returnCode == 0:
 		print("Successfully completed recording")
 		return
@@ -27,7 +32,14 @@ def record():
 		print("Recording was interrupted...")
 		Count += 1
 		record()
-	print("Return code: " + str(returnCode))
+
+def get_record_string():
+	global Seconds
+	recstring = "arecord -D plughw:1 --rate 140000"
+	if Seconds > 0:
+		recstring = recstring + " --duration " + str(Seconds)
+	recstring += recstring + " " + get_filename()
+	return recstring
 
 def does_file_exist(file):
 	return os.path.isfile("/home/"+file+"_0.wav") 
